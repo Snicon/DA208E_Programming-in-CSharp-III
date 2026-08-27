bec3c649e34bcdf7f0b58fe8de962542e2053408
Sixten Peterson <sixten@snicon.rip>
2026-08-27 13:00:14 +0200

    Add pages

---
+++ Graded Assignments/DA208E-Assignment1/DA208E-Assignment1/Pages/Guests/Edit.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DA208E_Assignment1.Pages.Guests;
+
+public class Edit : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
---
+++ Graded Assignments/DA208E-Assignment1/DA208E-Assignment1/Pages/Guests/Index.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DA208E_Assignment1.Pages.Guests;
+
+public class Index : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}

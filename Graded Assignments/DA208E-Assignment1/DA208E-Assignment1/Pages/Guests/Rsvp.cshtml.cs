2aee153fde24126f568a08106f3913bbf3637d2a
Sixten Peterson <sixten@snicon.rip>
2026-08-31 16:51:55 +0200

    Add basics of logic behind RSVP, some changes pending

--- Graded Assignments/DA208E-Assignment1/DA208E-Assignment1/Pages/Guests/Rsvp.cshtml.cs
+++ Graded Assignments/DA208E-Assignment1/DA208E-Assignment1/Pages/Guests/Rsvp.cshtml.cs
@@ -1,9 +1,33 @@
+using DA208E_Assignment1.Data;
+using DA208E_Assignment1.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace DA208E_Assignment1.Pages.Guests;
 
 public class Rsvp : PageModel
 {
+    private GuestService _guestService;
+    
+    [BindProperty]
+    public Guest Guest { get; set; }
+
+    public Rsvp(GuestService guestService)
+    {
+        _guestService = guestService;
+        Guest = new Guest();
+    }
+    
+    public IActionResult OnPost()
+    {
+        if (!ModelState.IsValid)
+            return Page();
+        
+        _guestService.Add(Guest);
+        
+        return RedirectToPage("Confirmation");
+    }
+    
     public void OnGet()
     {
         

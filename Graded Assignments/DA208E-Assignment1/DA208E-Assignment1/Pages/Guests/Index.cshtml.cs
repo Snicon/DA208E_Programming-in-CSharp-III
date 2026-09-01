d81c0b518217ceeccaa23723e1580270e16aab88
Sixten Peterson <sixten@snicon.rip>
2026-09-01 11:04:34 +0200

    Add table with guest data in Index.cshtml

--- Graded Assignments/DA208E-Assignment1/DA208E-Assignment1/Pages/Guests/Index.cshtml.cs
+++ Graded Assignments/DA208E-Assignment1/DA208E-Assignment1/Pages/Guests/Index.cshtml.cs
@@ -1,11 +1,22 @@
+using DA208E_Assignment1.Data;
+using DA208E_Assignment1.Models;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace DA208E_Assignment1.Pages.Guests;
 
 public class Index : PageModel
 {
+    private GuestService _guestService;
+    
+    public List<Guest> Guests { get; set; }
+
+    public Index(GuestService guestService)
+    {
+        _guestService = guestService;
+    }
+    
     public void OnGet()
     {
-        
+        Guests = _guestService.GetAll();
     }
 }

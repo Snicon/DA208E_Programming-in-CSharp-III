c0f5b2230d6a2b7f8b2a5ee2dfb15287131531a6
Sixten Peterson <sixten@snicon.rip>
2026-09-01 16:33:16 +0200

    Add edit support

--- Graded Assignments/DA208E-Assignment1/DA208E-Assignment1/Pages/Guests/Edit.cshtml.cs
+++ Graded Assignments/DA208E-Assignment1/DA208E-Assignment1/Pages/Guests/Edit.cshtml.cs
@@ -1,11 +1,38 @@
+using DA208E_Assignment1.Data;
+using DA208E_Assignment1.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace DA208E_Assignment1.Pages.Guests;
 
 public class Edit : PageModel
 {
-    public void OnGet()
+    private GuestService _guestService;
+    
+    [BindProperty]
+    public Guest Guest { get; set; }
+
+    public Edit(GuestService guestService)
     {
+        _guestService = guestService;
+    }
+    
+    public IActionResult OnGet(int id)
+    {
+        Guest = _guestService.Get(id);
+
+        if (Guest == null)
+            return RedirectToPage("Index");
         
+        return Page();
+    }
+
+    public IActionResult OnPost()
+    {
+        if (!ModelState.IsValid) // Validation check, failed
+            return Page();
+        
+        _guestService.Update(Guest);
+        return RedirectToPage("Index");
     }
 }

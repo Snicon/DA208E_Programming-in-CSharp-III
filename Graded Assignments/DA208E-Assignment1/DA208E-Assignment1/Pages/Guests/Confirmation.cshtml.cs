d6afc6aa95f3f7c3d52cf3c0e278d00a8a257f55
Sixten Peterson <sixten@snicon.rip>
2026-09-07 10:52:58 +0200

    Update re-use eventinfo object from DI

--- Graded Assignments/DA208E-Assignment1/DA208E-Assignment1/Pages/Guests/Confirmation.cshtml.cs
+++ Graded Assignments/DA208E-Assignment1/DA208E-Assignment1/Pages/Guests/Confirmation.cshtml.cs
@@ -4,6 +4,13 @@ namespace DA208E_Assignment1.Pages.Guests;
 
 public class Confirmation : PageModel
 {
+    public Models.Event EventInfo { get; set; }
+
+    public Confirmation(Models.Event eventInfo)
+    {
+        EventInfo = eventInfo;
+    }
+    
     public void OnGet()
     {
         

70751d90300c230d98759c07bd86585920288f94
Sixten Peterson <sixten@snicon.rip>
2026-09-14 12:02:34 +0200

    Update to get RSVP by date from EventInfo

--- Graded Assignments/DA208E-Assignment1/DA208E-Assignment1/Pages/Index.cshtml.cs
+++ Graded Assignments/DA208E-Assignment1/DA208E-Assignment1/Pages/Index.cshtml.cs
@@ -5,7 +5,15 @@ namespace DA208E_Assignment1.Pages;
 
 public class IndexModel : PageModel
 {
+    public Models.Event EventInfo { get; set; }
+
+    public IndexModel(Models.Event eventInfo)
+    {
+        EventInfo = eventInfo;
+    }
+    
     public void OnGet()
     {
+        
     }
 }
